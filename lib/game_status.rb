cb019c927f089db2aeacf589d4e0bc54b8bc06bf
Koy Saeteurn <kosatun@aol.com>
2018-07-13 03:09:41 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,8 +39,7 @@ end
 
 
 def over?(board)
-  if won?(board) == true || draw?(board) == true || 
-    full?(board) == true
+  if won?(board) == true || full?(board) == true
     return true
   end
 end  
