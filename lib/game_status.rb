0adaad9f626f0d93e9fd21ac4b6a529aa29d0f23
Koy Saeteurn <kosatun@aol.com>
2018-07-13 01:39:25 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,54 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combo|
+    win_combo.each do |value|
+      value.all? do |symbol|
+        if symbol == "X" || symbol == "O"
+          return win_combo  
+        else
+          false
+        end
+      end  
+    end  
+  end
+end  
+
+
+
+def full?(board)
+  position_taken(board, index).all? do |value2|
+    if value2 == "X" or value2 == "O"
+      return true 
+    else
+      return false 
+    end
+  end  
+end
+
+
+def draw?(board)
+  
+end
+
+
+def over?(board)
+  
+end  
+
+
+def winner(board)
+  
+end  
